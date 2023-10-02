c3dedf889abc6c773e9d2fd7dc631805bb9f0e0e
Hikaru <ito.hikaru@ci-quest.com>
2023-10-03 03:24:41 +0900

    ScanCode.js修正（りゅーきんspecialthx!）

--- src/components/ScanCode.js
+++ src/components/ScanCode.js
@@ -1,69 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import Quagga from 'quagga';
-import './ScanCode.css';
+import React, { useState, useEffect } from "react";
+import Quagga from "quagga";
+import "./ScanCode.css";
 
 const ScanCode = (props) => {
-  const [barcode, setBarcode] = useState('');
+  const [barcode, setBarcode] = useState("");
 
   useEffect(() => {
-    const initQuagga = async () => {
-      try {
-        // Quaggaの設定オブジェクトを定義
-        const config = {
-          inputStream: {
-            name: 'Live',
-            type: 'LiveStream',
-            target: document.querySelector('#preview'),
-            constraints: {
-              width: 450,
-              height: 200,
-            },
-            // 検出範囲の指定: 上下30%は対象外
-            area: { top: "30%", right: "0%", left: "0%", bottom: "30%" },
-            singleChannel: false,
-          },
-          locator: {
-            patchSize: 'medium',
-            halfSample: true,
-          },
-          decoder: {
-            readers: ['ean_reader'],
-          },
-          numOfWorkers: navigator.hardwareConcurrency || 4, // オプション名の誤りを修正
-          locate: true,
-        };
+    const config = {
+      inputStream: {
+        type: "LiveStream",
+        target: document.querySelector("#preview"),
+        constraints: {
+          width: { min: 450 },
+          height: { min: 300 },
+          facingMode: "environment",
+          aspectRatio: { min: 1, max: 2 },
+        },
+      },
+      locator: {
+        patchSize: "medium",
+        halfSample: true,
+      },
+      numOfWorkers: 2,
+      frequency: 10,
+      decoder: {
+        readers: ["ean_reader"],
+      },
+      locate: true,
+    };
 
-        // Quaggaを初期化
-        await Quagga.init(config);
-
-        // バーコードが検出された際の処理
-        Quagga.onDetected((result) => {
-          if (result && result.codeResult) {
-            const scannedBarcode = result.codeResult.code;
-            setBarcode(scannedBarcode);
-            props.onBarcodeScanned(scannedBarcode);
-          }
-        });
-
-        // Quaggaを開始
-        Quagga.start();
-      } catch (error) {
-        console.error('Quagga initialization error:', error);
+    Quagga.init(config, (err) => {
+      if (err) {
+        console.log(err, "error msg");
       }
-
-      // コンポーネントがアンマウントされた時にQuaggaを停止
+      Quagga.start();
       return () => {
         Quagga.stop();
       };
-    };
+    });
 
-    initQuagga();
-  }, [props]);
+    //detecting boxes on stream
+    Quagga.onProcessed((result) => {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            Number(drawingCanvas.getAttribute("width")),
+            Number(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(function (box) {
+              return box !== result.box;
+            })
+            .forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2,
+              });
+            });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2,
+          });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
+        }
+      }
+    });
+
+    // バーコードが検出された際の処理
+    // Quagga.onDetected((result) => {
+    //   if (result && result.codeResult) {
+    //     const scannedBarcode = result.codeResult.code;
+    //     setBarcode(scannedBarcode);
+    //     props.onBarcodeScanned(scannedBarcode);
+    //   }
+    // });
+
+    Quagga.onDetected(detected);
+  }, []);
+
+  const detected = (result) => {
+    console.log(result.codeResult.code);
+    if (result && result.codeResult) {
+      const scannedBarcode = result.codeResult.code;
+      setBarcode(scannedBarcode);
+      props.onBarcodeScanned(scannedBarcode);
+    }
+  };
 
   return (
     <div className="scan-code-container">
       <hr />
-      {barcode !== '' ? `バーコード：${barcode}` : 'スキャン中'}
+      {barcode !== "" ? `バーコード：${barcode}` : "スキャン中"}
       <hr />
       <div id="preview" className="preview"></div>
     </div>
